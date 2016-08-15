b941bf02b08eae52b054a63cd0b13c0141387ec6
pegasuspect <osman.sekerlen@ozu.edu.tr>
2016-08-15 23:38:26 +0300

    Fixed the logic a bit and improved visualization.

--- default.js
+++ default.js
@@ -2,21 +2,22 @@ $(function() {
   var container = $(".container");
   var r = container.width()/2;
   var numEl = $(".container li").length;
-  var oneDeg = Math.PI/360;
-  var interval = 360*2*oneDeg/numEl;
+  var oneDeg = Math.PI/180;
+  var interval = 360*oneDeg/numEl;
   var _startingDeg = 0*oneDeg;
 
   var placeObjectsAccordingTo = function(startingDeg) {
     $("ul").children().each(function(i, el) {
       var deg = interval*i + startingDeg;
-      $(el).css("top", r*(Math.sin(deg) + 1));
-      $(el).css("left", r*(Math.cos(deg) + 1));
+      var x = Math.floor(r*(Math.cos(deg)));
+      var y = Math.floor(r*(Math.sin(deg) + 1));
+      $(el).css("transform", "translate3d(" + x + "px, " + y + "px, 1px)" );
     });
   };
 
   placeObjectsAccordingTo(_startingDeg);
 
-  var timeFor1Interval = 100; //ms
+  var timeFor1Interval = 200; //ms
   var timeLimit = numEl*timeFor1Interval;
   var callbackDelay = timeFor1Interval/50; //ms
   var timeCallback = function() {
